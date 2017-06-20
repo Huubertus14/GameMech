c18e73e6dfdae33838645eaa7b26d5e8d34a9e2b
Huub van de Hoef <huub_thomas@hotmail.com>
2017-06-20 22:48:00 +0200

    Player Added

---
+++ Roaming Survival/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+
+    Vector3 playerPosition;
+    GameObject player;
+    float offset = 0.5f;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPosition = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerPosition = player.transform.position;
+        transform.position = new Vector3(playerPosition.x, playerPosition.y, -offset);
+	}
+}
---
+++ Roaming Survival/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    /*
+     * This script is used to detect the movement of the player
+     * This wil also detects any actions the player makes like chopping wood of attacking
+ 
+     */
+
+    private Rigidbody2D rb;
+    private float speed = 1;
+
+    //0 = down
+    //1 = up
+    //2 = left
+    //3 = rigth
+
+    private int state = 0;
+    private SpriteRenderer spr;
+    private int animationSpeed = 5;
+
+    public Sprite[] downSprites = new Sprite[4];
+    public Sprite[] upSprites = new Sprite[4];
+    public Sprite[] leftSprites = new Sprite[4];
+    public Sprite[] rightSprites = new Sprite[4];
+
+    public float animationCounter = 0;
+    // Use this for initialization
+    void Awake () {
+        spr = gameObject.GetComponentInChildren<SpriteRenderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        MovementSet();
+        PlayerAnimation();
+    }
+
+    private void PlayerAnimation()
+    {
+        //Check the state and call the animations for that kind of sprite set & state
+        if (state == 0)
+        {
+            FixedPlayerAnimation(downSprites);
+        }
+        else if (state == 1)
+        {
+            FixedPlayerAnimation(upSprites);
+        }
+        else if (state == 2)
+        {
+            FixedPlayerAnimation(leftSprites);
+        }
+        else if (state == 3)
+        {
+            FixedPlayerAnimation(rightSprites);
+        }
+
+    }
+    
+    private void FixedPlayerAnimation(Sprite[] sprites)
+    {
+        //down move animation
+        if (animationCounter > 3)
+        {
+            spr.sprite = sprites[3];
+        }
+        else if (animationCounter > 2)
+        {
+            spr.sprite = sprites[2];
+        }
+        else if (animationCounter > 1)
+        {
+            spr.sprite = sprites[1];
+        }
+        else if (animationCounter > 0)
+        {
+            spr.sprite = sprites[0];
+        }
+    }
+
+
+    void MovementSet()
+    {
+        if (Input.GetKey("w"))
+        {
+            //Up
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1, 0);
+            state = 1;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            //Left
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, 0);
+            state = 2;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            //Down
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, 0);
+            state = 0;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            //Right
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, 0);
+            state = 3;
+        }
+
+        if(!Input.GetKey("d") && !Input.GetKey("w") && !Input.GetKey("s") && !Input.GetKey("a"))
+        {
+            animationCounter = 0;
+        }else
+        {
+            animationCounter += animationSpeed * Time.deltaTime;
+            if(animationCounter > 3.9f)
+            {
+                animationCounter = 0;
+            }
+        }
+    }
+
+    void MovementForce()
+    {
+        if (Input.GetKey("w"))
+        {
+            //Up
+           rb.AddForce(Vector2.up * speed);
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            //Left
+            rb.AddForce(-Vector2.right * speed);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            //Down
+            rb.AddForce(-Vector2.up * speed);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            //Right
+            rb.AddForce(Vector2.right * speed);
+        }
+    }
+}
